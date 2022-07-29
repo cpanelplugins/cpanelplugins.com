1333365d23f3ef41e648f92a0f6ed0afc82811cb
stefanpejcic <pejcha1994@gmail.com>
2022-07-29 08:18:36 +0200

    contributors added

--- src/pages/Developers.vue
+++ src/pages/Developers.vue
@@ -4,13 +4,20 @@
       <h1 class="text-center">Hire cPanel Plugin Developers</h1>
 
 <p>Looking for an experienced cPanel plugin developer to help you customize an existing plugin or create a new one?</p>
-<p>Here is a list of freelance cPanel plugin developers that have already published free cPanel plugins:</p>
+<p>Here is a list of experienced cPanel plugin developers that have already published useful cPanel plugins:</p>
           <ul>
 		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/taidos/">Carlos Faustino</a></li>
 		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/ismaelyws/">Hans Desjarlais</a></li>
 		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/stefanpejcic/">Stefan Pejcic</a></li>
+		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/petermw/">Peter Ivanov</a></li>
 		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/fevangelou/">Fotis Evangelou</a></li>
 		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/felipegabriel/">Felipe Gabriel</a></li>
+		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/rojo2530/">Jose Cantos</a></li>
+		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/ismaelyws/">Hans Desjarlais</a></li>
+		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/timelord/">Marcel Pol</a></li>
+		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/lucyfer622/">Jarnick Puaw</a></li>
+		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/RVGlobalsoft/">RVGlobalsoft</a></li>
+		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/djfordz/">David Ford</a></li>
 		 </ul>
     </Section>
   </Layout>
