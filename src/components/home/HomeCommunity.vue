bfab99a233d60c9010735f95d5f7be529ab1a7f3
Stefan Pejcic <pejcha1994@gmail.com>
2022-10-01 18:35:32 +0200

    Update HomeCommunity.vue

--- src/components/home/HomeCommunity.vue
+++ src/components/home/HomeCommunity.vue
@@ -3,11 +3,11 @@
   	<h2 class="text-center mb-x2">Looking for quality cPanel and WordPress tutorials?</h2>
     <div class="grid-cols container container-md grid-cols--2">
     	<Card class="sponsor text-center">
-	      <a href="https://pcx3.com/" target="_blank"><g-image src="~/assets/images/pcx3.png" alt="PCX3.com"/></a>
+	      <a href="https://pcx3.com/" target="_blank"><h1>PC✘3</h1></a>
 	      <p>Guides on how to solve common cPanel and Linux problems.</p>
     	</Card>
       <Card class="sponsor text-center">
-	      <a href="https://wpxss.com/" target="_blank"><g-image src="~/assets/images/wpxss.png" alt="WPXSS.com"/></a>
+	      <a href="https://wpxss.com/" target="_blank"><h1>wp✘SS</h1></a>
 	      <p>WordPress tutorials and instructions for advanced users.</p>
     	</Card>
     </div>
