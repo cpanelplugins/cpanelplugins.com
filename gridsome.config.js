acce155f9ed71bb731e0167e8832401686a5122f
Stefan Pejcic <pejcha1994@gmail.com>
2022-10-05 14:29:51 +0200

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -45,6 +45,24 @@ module.exports = {
       }
     },
     {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        exclude: ['/exclude-me'],
+        config: {
+          '/plugins/*': {
+            changefreq: 'weekly',
+            priority: 0.5,
+            lastmod: '2020-02-19',
+          },
+          '/blog/*': {
+            changefreq: 'monthly',
+            priority: 0.7,
+            lastmod: '2020-05-12',
+          }
+        }
+      }
+    },
+    {
       use: '@gridsome/plugin-critical',
       options: {
         paths: ['/'],
