90762046f158c923e404c728f143e751c3e10f22
Stefan Pejcic <pejcha1994@gmail.com>
2022-10-08 09:14:09 +0200

    Update HomeIntroSimple.vue

--- src/components/home/HomeIntroSimple.vue
+++ src/components/home/HomeIntroSimple.vue
@@ -13,7 +13,7 @@
         <p class="intro__links">
           <g-link  to="/plugins/platform/cp/" class="button primary button--large">
             <span>cPanel plugins</span>
-          </g-link>
+          </g-link>&nbsp;&nbsp;
           <g-link  to="/plugins/platform/whm/" class="button secondary button--large">
             <span>WHM plugins</span>
           </g-link>
