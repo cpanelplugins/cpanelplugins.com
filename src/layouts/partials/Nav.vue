2c5ad30a37476343321042b9cada50a787caef09
Stefan Pejcic <pejcha1994@gmail.com>
2022-10-11 10:57:48 +0200

    Update Nav.vue

--- src/layouts/partials/Nav.vue
+++ src/layouts/partials/Nav.vue
@@ -5,9 +5,25 @@
       <span class="main-nav__label">Home</span>
     </g-link>
     
-    <g-link to="/plugins/">
-      <span class="main-nav__label">Plugins</span>
-    </g-link>
+    <div class="has-dropdown">
+          <a
+            aria-label="GitHub"
+            href="/plugins/"
+            rel="noopener noreferrer"
+            target="_blank"
+            title="Plugins"
+          ><span
+             style="margin-left:5px;"
+            >Plugins</span>
+          </a>
+          <div class="dropdown">
+            <a href="/plugins/platform/cp/" rel="noopener noreferrer" target="_blank">
+              cPanel Plugins
+            </a>
+            <a href="/plugins/platform/whm/" rel="noopener noreferrer" target="_blank">
+              WHM Plugins
+            </a>
+          </div>
   
 	<g-link to="/developers/">
       <span class="main-nav__label">Developers<sup> <b style="color:var(--primary-color)"> NEW</b></sup></span>
@@ -20,31 +36,6 @@
 	<g-link to="/about/">
       <span class="main-nav__label">About</span>
     </g-link>
-    
-    
-    
-    <div class="has-dropdown">
-          <a
-            aria-label="GitHub"
-            href="/docs"
-            rel="noopener noreferrer"
-            target="_blank"
-            title="Gridsome @ GitHub"
-          ><span
-             style="margin-left:5px;"
-            >Support</span>
-          </a>
-          <div class="dropdown">
-            <a href="/docs" rel="noopener noreferrer" target="_blank">
-              Documentation
-            </a>
-            <a href="//github.com/gridsome/gridsome/issues" rel="noopener noreferrer" target="_blank">
-              FAQ
-            </a>
-             <a href="//github.com/gridsome/gridsome/projects/2" rel="noopener noreferrer" target="_blank">
-              Open Ticket
-            </a>
-          </div>
     
     
   </nav>
