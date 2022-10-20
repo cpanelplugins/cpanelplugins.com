2d779d34cdfcf04230181f12c95a13523c7781e8
Stefan Pejcic <pejcha1994@gmail.com>
2022-10-20 07:01:13 +0200

    Update Developers.vue

--- src/pages/Developers.vue
+++ src/pages/Developers.vue
@@ -5,20 +5,35 @@
 
 <p>Looking for an experienced cPanel plugin developer to help you customize an existing plugin or create a new one?</p>
 <p>Here is a list of experienced cPanel plugin developers that have already published useful cPanel plugins:</p>
-          <ul>
-		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/taidos/">Carlos Faustino</a></li>
-		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/ismaelyws/">Hans Desjarlais</a></li>
-		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/stefanpejcic/">Stefan Pejcic</a></li>
-		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/petermw/">Peter Ivanov</a></li>
-		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/fevangelou/">Fotis Evangelou</a></li>
-		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/felipegabriel/">Felipe Gabriel</a></li>
-		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/rojo2530/">Jose Cantos</a></li>
-		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/ismaelyws/">Hans Desjarlais</a></li>
-		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/timelord/">Marcel Pol</a></li>
-		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/lucyfer622/">Jarnick Puaw</a></li>
-		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/RVGlobalsoft/">RVGlobalsoft</a></li>
-		  <li style="margin-bottom: 0px;"><a class="menu-item" href="/contributor/djfordz/">David Ford</a></li>
-		 </ul>
+<table class="table">
+  
+  <tbody>
+    <tr>
+      <td><a class="menu-item" href="/contributor/taidos/">Carlos Faustino</a></td>
+      <td><a class="menu-item" href="/contributor/ismaelyws/">Hans Desjarlais</a></td>
+      <td><a class="menu-item" href="/contributor/petermw/">Peter Ivanov</a></td>
+    </tr>
+    
+    
+  <tr>
+      <td><a class="menu-item" href="/contributor/stefanpejcic/">Stefan Pejcic</a></td>
+      <td><a class="menu-item" href="/contributor/fevangelou/">Fotis Evangelou</a></td>
+      <td><a class="menu-item" href="/contributor/felipegabriel/">Felipe Gabriel</a></td>
+    </tr>
+    
+    <tr>
+      <td><a class="menu-item" href="/contributor/rojo2530/">Jose Cantos</a></td>
+      <td><a class="menu-item" href="/contributor/ismaelyws/">Hans Desjarlais</a></td>
+      <td><a class="menu-item" href="/contributor/timelord/">Marcel Pol</a></td>
+    </tr>
+    
+    <tr>
+      <td><a class="menu-item" href="/contributor/lucyfer622/">Jarnick Puaw</a></td>
+      <td><a class="menu-item" href="/contributor/RVGlobalsoft/">RVGlobalsoft</a></td>
+      <td><a class="menu-item" href="/contributor/djfordz/">David Ford</a></td>
+    </tr>
+    </tbody>
+</table>
     </Section>
   </Layout>
 </template>
