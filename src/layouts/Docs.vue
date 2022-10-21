052a1dcd49f6a6b736e9e7a9dba82ca5d4984ad2
Stefan Pejcic <pejcha1994@gmail.com>
2022-10-21 20:49:22 +0200

    Update Docs.vue

--- src/layouts/Docs.vue
+++ src/layouts/Docs.vue
@@ -1,17 +1,7 @@
 <template>
   <Layout class="has-sidebar docs-page" :footer="false">
     <div class="container flex flex-align-top">
-      <div class="sidebar">
-        <template v-if="links" v-for="(group, i1) in links">
-          <h3 class="menu-item" :key="`title-${i1}`">{{ group.title }}</h3>
-          <template v-for="(item, i2) in group.items">
-            <g-link :exact="item.link == '/exams/'" class="menu-item menu-link" :to="item.link" :key="`link-${i1}-${i2}`">
-              {{ item.title }}
-            </g-link>
-          </template>
-        </template>
-      </div>
-      <Section class="doc-content flex-fit" container="base">
+      <Section class="doc-content flex-fit" container="">
         <slot></slot>
         <p>
           <a :href="editLink" target="_blank" class="github-edit-link">
@@ -32,16 +22,6 @@
           </div>
         </nav>
       </Section>
-      <div v-if="subtitles.length > 0 && subtitles[0].depth !== 3" class="sidebar sidebar--right hide-for-small">
-        <h3>On this page</h3>
-        <ul v-if="subtitles.length" class="menu-item submenu">
-          <li class="submenu__item" :class="'submenu__item-depth-' + subtitle.depth" v-for="subtitle in subtitles" :key="subtitle.value">
-            <a class="submenu__link" :href="subtitle.anchor">
-              {{ subtitle.value }}
-            </a>
-          </li>
-        </ul>
-      </div>
     </div>
   </Layout>
 </template>
