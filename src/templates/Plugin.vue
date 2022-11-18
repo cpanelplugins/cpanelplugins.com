daa373f26e197b64b63d906b9c1d0ff6e3cbc351
Stefan Pejcic <pejcha1994@gmail.com>
2022-11-18 12:34:37 +0100

    Update Plugin.vue

--- src/templates/Plugin.vue
+++ src/templates/Plugin.vue
@@ -50,7 +50,7 @@
               </p>
               <hr />
               <div class="deploy-buttons flex">
-                <a class="button button--small" style="width: 100%; text-align: center; display:block;" href="mailto:stefan@cpanelplugins.com">
+                <a class="button button--small" style="width: 100%; text-align: center; display:block;" href="mailto:info@cpanelplugins.com">
                   <cPanelLogo alt="cPanel plugin installation service" height="16" /> cPanel plugin installation service - $9.95
                 </a>
               </div>
